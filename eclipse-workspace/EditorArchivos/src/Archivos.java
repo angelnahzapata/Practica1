900fa36a5f1ba965f455f11c87433355047182be
Angel Nah <nah834086@gmail.com>
2024-02-27 10:09:09 -0600

    Agregué otra linea de impresion con los nombres de mis compañeros

--- eclipse-workspace/EditorArchivos/src/Archivos.java
+++ eclipse-workspace/EditorArchivos/src/Archivos.java
@@ -2,10 +2,16 @@
 public class Archivos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		
 			System.out.print("Angel Nah Zapata");
+			
+			System.out.print("\n");
+			
+			System.out.print("Enrique Chan Díaz, Guidnell Pérez Cruz, Diego Avilés, Ricardo Chab");
+			
+			
+			
 	}
 
 }
